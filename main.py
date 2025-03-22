d91632e0af9b265880bbbf7a5f87fe6e742aa935
nsulonen <niko.sulonen@proton.me>
2025-03-22 13:15:58 +0200

    update main.py and stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, sort_list
+import sys
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as file:
@@ -6,8 +7,27 @@ def get_book_text(filepath: str) -> str:
     return file_contents
 
 def main() -> None:
-    print(count_words(get_book_text("books/frankenstein.txt")))
-    print(count_chars(get_book_text("books/frankenstein.txt")))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath: str = sys.argv[1]
+    text: str = get_book_text(filepath)
+    word_count: str = count_words(text)
+    sorted_list: dict = sort_list(count_chars(text))
+
+    print(
+        "============ BOOKBOT ============",
+        f"\nAnalyzing book found at {filepath}...",
+        "\n----------- Word Count ----------",
+        f"\nFound {word_count} total words",
+        "\n--------- Character Count -------",
+        f"\n{str(sorted_list)
+        .replace(", ", "\n")
+        .replace("'", "")
+        .replace("{", "")
+        .replace("}", "")}"
+    )
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,15 +2,21 @@ def count_words(text: str) -> str:
     words: int = 0
     for word in text.split():
         words += 1
-    return f"{words} words found in the document"
+    return words
 
 def count_chars(text: str) -> dict:
     char_count: dict = {}
     for word in text.split():
         for char in word:
             char = char.lower()
-            if char in char_count:
+            if char in char_count and char.isalpha():
                 char_count[char] += 1
-            else:
+            elif char.isalpha():
                 char_count[char] = 1
+            else:
+                pass
     return char_count
+
+def sort_list(char_count: dict) -> dict:
+    sorted_list: dict = {key: value for key, value in sorted(char_count.items(), key=lambda item: item[1], reverse=True)}
+    return sorted_list
